31e96034b0108fb985accc37c3c16b8da630c3d8
Marina Gabriela <marina_zaeta@hotmail.com>
2023-11-12 08:26:46 -0300

    ejercicio Lombok

---
+++ Clase9/src/main/java/modelo/Persona.java
@@ -0,0 +1,16 @@
+package modelo;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+	private String nombre;
+	private String apellido;
+	private String fechaNacimiento;
+	
+	//constructor
+	//getters y setters
+	//método to string
+	//método equals
+
+}
